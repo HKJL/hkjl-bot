8705eccee781415a66c795cafc4db0e1dafcc23f
Mathijs Schmittmann <info@mathijs.info>
2016-11-06 13:23:58 +0100

    discourse latest topic checker now also keeps track of forum online/offline status

--- mod_discourse.php
+++ mod_discourse.php
@@ -24,32 +24,56 @@ function discourse_latest() {
 
     // Load file containing $discourse_api_key value
     include("mod_discourse_config.php");
-
-    $json = file_get_contents("https://community.hackenkunjeleren.nl/latest.json?api_key=".$discourse_api_key."&api_username=Test");
-    $results = json_decode($json, 1);
-  
-    // Skip pinned topics
-    $i = 0;
-    while($results["topic_list"]["topics"][$i]["pinned"]=="true") {
-        $i++;
-    }
-
-    $text = "⭐  \"".$results["topic_list"]["topics"][$i]["title"]."\" door ".$results["topic_list"]["topics"][$i]["last_poster_username"]. " - ";
-    $url = "https://community.hackenkunjeleren.nl/t/".$results["topic_list"]["topics"][$i]["slug"]."/".$results["topic_list"]["topics"][$i]["id"]."/".$results["topic_list"]["topics"][$i]["highest_post_number"];
-
     include("sqlconfig.php");
-    $dbh = new PDO('mysql:host=localhost;dbname='.$db,$user,$pass,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")); 
-    $query = $dbh->prepare("SELECT value FROM storage WHERE name='discourse-last-reported-url'");
-    $query->execute();
-    $latestreportedurl = $query->fetchColumn();
 
-    if($latestreportedurl == $url) {
-        return "";
-    } else { 
-        $query = $dbh->prepare("UPDATE storage SET value=:url WHERE name='discourse-last-reported-url'");
-        $query->bindParam(':url',$url,PDO::PARAM_STR);
+    $dbh = new PDO('mysql:host=localhost;dbname='.$db,$user,$pass,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    $json = file_get_contents("https://community.hackenkunjeleren.nl/latest.json?api_key=".$discourse_api_key."&api_username=Test");
+
+    if($json === FALSE) {
+        // Forum JSON is unreachable
+        $query = $dbh->prepare("SELECT value FROM storage WHERE name='discourse-online'");
         $query->execute();
-        return "[Discourse] ".$text.$url;
-    }
+        $discourseonline = $query->fetchColumn();
+        if($discourseonline=="true") {
+            $query = $dbh->prepare("UPDATE storage SET value='false' WHERE name='discourse-online'");
+            $query->execute();
+            return "[Discourse] Forum is OFFLINE!";
+        } else {
+            return "";
+        }
+    } else {
+        // Forum JSON is reachable
+        $query = $dbh->prepare("SELECT value FROM storage WHERE name='discourse-online'");
+        $query->execute();
+        $discourseonline = $query->fetchColumn();
+        if($discourseonline=="false") {
+            $query = $dbh->prepare("UPDATE storage SET value='true' WHERE name='discourse-online'");
+            $query->execute();
+            return "[Discourse] Forum doet het weer! \o/";
+        } else {
+            $results = json_decode($json, 1);
 
+            // Skip pinned topics
+            $i = 0;
+            while($results["topic_list"]["topics"][$i]["pinned"]=="true") {
+                $i++;
+            }
+
+            $text = "⭐  \"".$results["topic_list"]["topics"][$i]["title"]."\" door ".$results["topic_list"]["topics"][$i]["last_poster_username"]. " - ";
+            $url = "https://community.hackenkunjeleren.nl/t/".$results["topic_list"]["topics"][$i]["slug"]."/".$results["topic_list"]["topics"][$i]["id"]."/".$results["topic_list"]["topics"][$i]["highest_post_number"];
+
+            $query = $dbh->prepare("SELECT value FROM storage WHERE name='discourse-last-reported-url'");
+            $query->execute();
+            $latestreportedurl = $query->fetchColumn();
+
+            if($latestreportedurl == $url) {
+                return "";
+            } else {
+                $query = $dbh->prepare("UPDATE storage SET value=:url WHERE name='discourse-last-reported-url'");
+                $query->bindParam(':url',$url,PDO::PARAM_STR);
+                $query->execute();
+                return "[Discourse] ".$text.$url;
+            }
+        }
+    }
 }
