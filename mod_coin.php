f82651c04d31f79f24dc14c8ad2e3f702a733fe1
Mathijs Schmittmann <info@mathijs.info>
2017-06-03 22:58:44 +0200

    Change mod_coin API to something that supports more cryptocurrencies

--- mod_coin.php
+++ mod_coin.php
@@ -2,19 +2,16 @@
 
 function coin($args) {
     $from = strtoupper($args);
-    $resultusd = @file_get_contents("https://apiv2.bitcoinaverage.com/convert/global?from=" . urlencode($from) . "&to=USD&amount=1");
-    if(!$resultusd) {
-        $returnstring = "[COIN] Ongeldige valuta, probeer eens BTC of ETH";    
+    $results = @file_get_contents("https://min-api.cryptocompare.com/data/price?fsym=" . urlencode($from) . "&tsyms=USD,EUR");
+    if(!$results) {
+        $returnstring = "[COIN] Er ging iets stuk...";
     } else {
-        $resulteur = @file_get_contents("https://apiv2.bitcoinaverage.com/convert/global?from=" . urlencode($from) . "&to=EUR&amount=1");
-        $resultusd = json_decode($resultusd, true);
-        $resulteur = json_decode($resulteur, true);
-        if($resultusd["success"] =! "ok" || $resulteur["success"] =! "ok") {
-            $returnstring = "[COIN] Er is iets mis met de API...";
+        $results = json_decode($results, true);
+        if(!array_key_exists("USD", $results)) {
+            $returnstring = "[COIN] Deze cryptocoin ken ik niet...";
         } else {
-            $returnstring = "[COIN] " . $from . " waarde is op dit moment: $ " . $resultusd["price"] . " / € " . $resulteur["price"];
+            $returnstring = "[COIN] " . $from . " waarde is op dit moment: $ " . $results["USD"] . " / € " . $results["EUR"];
         }
     }
     return $returnstring;
 }
-
