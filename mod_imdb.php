12ddcf29390d7217347fde660d1e72a7815a46b9
Cas de Reuver <cas@reuver.co>
2017-06-21 21:52:07 +0200

    Revert "Move IMDB api form omdbapi.com -> imdb.com"
    This reverts commit 4c9e748cc4d1ef87463280a80d10429e0a03878a.

--- mod_imdb.php
+++ mod_imdb.php
@@ -3,17 +3,13 @@
 function imdb($args) {
 
     if(strlen($args)>1) {
-        $search = preg_replace('/[^A-Za-z0-9]/', '', preg_replace('/\s/', '_', strtolower($args)));
-        $result = file_get_contents("https://v2.sg.media-imdb.com/suggests/a/".urlencode($search).".json");
-        $result = preg_replace('/^imdb.*\(/', '', $result);
-        $result = json_decode(rtrim($result, ')'), true);
+        $result = json_decode(file_get_contents("http://www.omdbapi.com/?t=".urlencode($args)),true);
         if($result == NULL) {
-            $returnstring = "[IMDB] Error: www.imdb.com not available!";
-        } elseif(count($result['d']) < 1) {
-            $returnstring = "[IMDB] Geen resultaat";
+            $returnstring = "[IMDB] Error: www.omdbapi.com not available!";
+        } elseif($result["Response"]=="False") {
+            $returnstring = "[IMDB] Error: ".$result["Error"];
         } else {
-            $result = $result['d'][0];
-            $returnstring = "[IMDB] http://www.imdb.com/title/".$result["id"]." - ".$result["l"].", ".$result["y"];
+            $returnstring = "[IMDB] http://www.imdb.com/title/".$result["imdbID"]." - ".$result["Title"].", ".$result["Year"]." (".$result["imdbRating"].") - ".$result["Plot"];
         }
     } else {
         $returnstring = "[IMDB] Error: Geef een zoekterm van 2 of meer tekens!";
